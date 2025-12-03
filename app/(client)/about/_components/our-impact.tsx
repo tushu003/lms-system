487d9af6c04a1c5e246ee0c1aa13b8abee1ce7ba
tushu003 <tahminata2015@gmail.com>
2025-12-03 21:24:54 +0600

    added tutor banner

--- app/(client)/about/_components/our-impact.tsx
+++ app/(client)/about/_components/our-impact.tsx
@@ -3,9 +3,9 @@ import React from "react";
 export default function OurImpact() {
   return (
     <section className="w-full">
-      <div className=" py-10 rounded-t-lg bg-gradient-to-r from-[#4F46E5] via-[#9333EA] to-[#DB2777]">
+      <div className="relative py-10 rounded-t-lg bg-gradient-to-r from-[#4F46E5] via-[#9333EA] to-[#DB2777]">
         {/* Heading */}
-         <div className="text-center mt-4 mb-3">
+        <div className="text-center mt-4 mb-3">
           <span className="inline-block bg-white/10 text-white text-sm font-medium px-3 py-1 rounded-full">
             Our Impact
           </span>
@@ -23,35 +23,26 @@ export default function OurImpact() {
             <div className="text-3xl font-bold text-white rounded-md  items-center justify-center">
               20+
             </div>
-            <p className="text-xs text-white/70">
-              Students Helped
-            </p>
+            <p className="text-xs text-white/70">Students Helped</p>
           </article>
           <article className="bg-white/10 backdrop-blur-sm rounded-2xl p-5 flex flex-col items-center gap-1">
             <div className="text-3xl font-bold text-white rounded-md  items-center justify-center">
               1+
             </div>
-            <p className="text-xs text-white/70">
-              Expert Tutors
-            </p>
+            <p className="text-xs text-white/70">Expert Tutors</p>
           </article>
           <article className="bg-white/10 backdrop-blur-sm rounded-2xl p-5 flex flex-col items-center gap-1">
             <div className="text-3xl font-bold text-white rounded-md  items-center justify-center">
               98+
             </div>
-            <p className="text-xs text-white/70">
-              Success Rate
-            </p>
+            <p className="text-xs text-white/70">Success Rate</p>
           </article>
           <article className="bg-white/10 backdrop-blur-sm rounded-2xl p-5 flex flex-col items-center gap-1">
             <div className="text-3xl font-bold text-white rounded-md  items-center justify-center">
               5+
             </div>
-            <p className="text-xs text-white/70">
-              Coutries Served
-            </p>
+            <p className="text-xs text-white/70">Coutries Served</p>
           </article>
-
         </div>
       </div>
     </section>
---
+++ app/(client)/find-tutor/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import TutorBanner from './_components/tutor-banner'
+
+export default function FindTutor() {
+  return (
+    <div>
+      <TutorBanner />
+    </div>
+  )
+}
