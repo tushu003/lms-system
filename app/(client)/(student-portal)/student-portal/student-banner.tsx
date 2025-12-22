014755c22a444fbcce50bf65bd063e193a01a52b
tushu003 <tahminata2015@gmail.com>
2025-12-23 00:11:35 +0600

    updated banner

--- app/(client)/(student-portal)/student-portal/student-banner.tsx
+++ app/(client)/(student-portal)/student-portal/student-banner.tsx
@@ -3,7 +3,8 @@ import React from "react";
 export default function StudentBanner() {
   return (
     <div className="bg-gradient-to-br from-[#312E81] via-[#581C87] to-[#831843] py-12">
-      <div className="max-w-3xl mx-auto px-4 text-center">
+     <div className="max-w-5xl mx-auto px-4 flex flex-col md:flex-row justify-between gap-8">
+         <div className="max-w-3xl ">
         <span className="inline-flex items-center justify-center px-3 py-1 rounded-full text-sm bg-white/10 text-white mb-4">
           Our Story
         </span>
@@ -21,6 +22,14 @@ export default function StudentBanner() {
           accessible, <br />personalized and Effective
         </p>
       </div>
+      <div className="flex gap-2">
+        <div>klj</div>
+        <div>
+            <p>Next Session in</p>
+            <p>20 hours</p>
+        </div>
+      </div>
+     </div>
     </div>
   );
 }
