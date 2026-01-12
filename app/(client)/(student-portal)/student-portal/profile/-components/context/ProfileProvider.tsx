a7899a31e6b3f8ceebafa18f99920e01b96c6c19
tushu003 <tahminata2015@gmail.com>
2026-01-13 01:41:32 +0600

    solve provider issues

---
+++ app/(client)/(student-portal)/student-portal/profile/-components/context/ProfileProvider.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import React from "react";
+import { ProfileProvider } from "./profile-context";
+
+export default function AppProfileProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <ProfileProvider>{children}</ProfileProvider>;
+}
