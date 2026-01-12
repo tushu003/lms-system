e08cc4fdd5267bb6ac16bf4e97b7e5bb9a87aa23
tushu003 <tahminata2015@gmail.com>
2026-01-12 23:01:29 +0600

    added profile card on profile page

--- app/(client)/(student-portal)/student-portal/profile/page.tsx
+++ app/(client)/(student-portal)/student-portal/profile/page.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import ProfileViewCard from './-components/ProfileViewCard'
 
 export default function Profile() {
   return (
     <div>
-        <
+        <ProfileViewCard />
     </div>
   )
 }
