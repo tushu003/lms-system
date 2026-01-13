dd03f60b7e716037546458d4b3aba90aeeeb9c9e
tushu003 <tahminata2015@gmail.com>
2026-01-13 15:03:23 +0600

    solve some issues

--- app/(client)/(student-portal)/student-portal/layout.tsx
+++ app/(client)/(student-portal)/student-portal/layout.tsx
@@ -1,7 +1,9 @@
-"use client";
-
 import { ProfileProvider } from "@/app/(client)/(student-portal)/student-portal/profile/-components/context/profile-context";
 
-export default function Providers({ children }: { children: React.ReactNode }) {
+export default function StudentPortalLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return <ProfileProvider>{children}</ProfileProvider>;
 }
--- app/(client)/(student-portal)/student-portal/profile/-components/context/ProfileProvider.tsx
+++ app/(client)/(student-portal)/student-portal/profile/-components/context/ProfileProvider.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import { ProfileProvider } from "./profile-context";
+import { ProfileProvider } from "";
 
 export default function AppProfileProvider({
   children,
