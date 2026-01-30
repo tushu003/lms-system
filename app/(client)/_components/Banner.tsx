8be3f6186206b44231d73836f2470c3cd1ad9173
tushu003 <tahminata2015@gmail.com>
2026-01-30 21:47:51 +0600

    updated bannar

--- app/(client)/_components/Banner.tsx
+++ app/(client)/_components/Banner.tsx
@@ -4,10 +4,10 @@ import bannerImage from '@/public/assets/hero/banner.png';
 
 export default function Banner() {
   return (
-    <div className=''>
+    <div className='max-w-[1920px] w-full '>
       <Image src={bannerImage}
       alt='banner'
-      width={1480}
+      width={1920}
       height={571} />
     </div>
   )
